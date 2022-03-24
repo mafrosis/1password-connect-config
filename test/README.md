0c8d16366842522f8d389fbd2deff15d6261e901
Matt Black <github@mafro.net>
2022-03-24 20:36:47 +1100

    Added a python test script

---
+++ test/README.md
@@ -0,0 +1,6 @@
+1Password Connect test script
+==========
+
+A simple python script using the
+[1Password/connect-sdk-python](https://github.com/1Password/connect-sdk-python) to pull secrets from
+a 1Password Connect instance.
---
+++ test/fetch_secret.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+import os
+
+from onepasswordconnectsdk.client import new_client
+
+CONNECT_HOST='http://ringil:8081'
+VAULT_ID='b6hmle4wvz2gl4mv6j3y4lcwza'
+
+def main():
+    connect_token = os.environ.get('OP_CONNECT_TOKEN')
+
+    client = new_client(url=CONNECT_HOST, token=connect_token)
+
+    data = {}
+
+    for item in client.get_items(VAULT_ID):
+        item = client.get_item(item.id, item.vault.id)
+
+        title = item.title.lower().replace('.', '_').replace(' ', '_')
+        password = next(iter([x.value for x in item.fields if x.id == 'password']))
+
+        data[title] = password
+
+    print(data)
+
+
+if __name__ == '__main__':
+    main()
